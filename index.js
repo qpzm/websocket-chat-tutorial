d19f08c510d4265321a0dc4ec71cd3d6e649d1ad
qpzm <lhm1442@gmail.com>
2020-07-11 16:36:36 +0900

    feat: broadcast messages

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const WebSocket = require('ws');
 const wsServer = new WebSocket.Server({ port: 8080 });
 
 wsServer.on('connection', function connection(ws) {
-  console.log(wsServer.clients);
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send(message);
+    wsServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
 });
